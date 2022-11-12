ffbdb83bcebee43eb2a1f8b60c691628151d82fe
Leonardo Marinovic <Leonardo.leo.201@gmail.com>
2022-11-12 23:03:45 +0100

    Added map sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,26 @@
 #include <Arduino.h>
 #include "koljenasto/Koljenasto.h"
+#include "map/MapSensor.h"
 
 const uint8_t PIN_MAP = A0;  
 const uint8_t PIN_KOLJENASTO = 2;
 const uint8_t PIN_BREAGASTA = 3;
 
 Koljenasto* koljenasto;
+MapSensor* mapSensor;
 
 void setup() 
 {
-  pinMode(PIN_MAP, INPUT); 		
+  		
   pinMode(PIN_BREAGASTA, INPUT);
 
   koljenasto = new Koljenasto(PIN_KOLJENASTO);
+  mapSensor = new MapSensor(PIN_MAP);
 
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println(koljenasto->getRPM());
+  //Serial.println(koljenasto->getRPM());
+  Serial.println(mapSensor->getPressure());
 }
