04ce3ea2d98bbc1619ecd4d95b8fe8ddeda6ccf6
lsagolj <sagoljluka@gmail.com>
2022-11-12 15:46:34 +0100

    Testni serial print code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup() 
+{
+   Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  Serial.print("Test");
+
+  delay(100);
 }
