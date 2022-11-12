14ba895fa947442d0e8776cff055adbe68a3ec25
Leonardo Marinovic <Leonardo.leo.201@gmail.com>
2022-11-12 19:26:36 +0100

    Glupost

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,9 @@ void loop() {
   int val = digitalRead(INPUT_KOLJ);
   if (val == 1 && prev == 0) {
     unsigned long speed = micros() - prev_time;
-    Serial.println(speed);
+    //Serial.println(speed);
     prev_time = micros();
   }
-
+  Serial.println(val);
   prev = val;
 }
