3dff72ada8a8719eaae4e8d51cc0ef4c76e1db2a
lsagolj <sagoljluka@gmail.com>
2022-11-12 19:20:07 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include <Arduino.h>
 #include <Timer.h>
 
-  const int A = A0;  
-  const int koljeno = 5;
+const int A = A0;  
+const int koljeno = 2;
 
-  int ulaz = 0;
-  int koljeno_val;
-  int last_val = 0;
-  float napon = 0;
-  long int proslo_vrijeme = 0;
+int ulaz = 0;
+int koljeno_val;
+int last_val = 0;
+float f_min;
+float napon = 0;
+uint32_t proslo_vrijeme = 0;
 
 Timer timer(MICROS);
 
@@ -18,54 +19,39 @@ void setup()
 {
   pinMode(koljeno, INPUT); 	
   pinMode(A, INPUT); 	
+
+  attachInterrupt(digitalPinToInterrupt(koljeno), rpm, RISING);
+
+
   Serial.begin(9600);
 }
 
 void loop() 
 {
-  ulaz = analogRead(A);
-  napon = ((float)ulaz / 1023.f) * 5.f;
-  koljeno_val = digitalRead(koljeno);
+  /*ulaz = analogRead(A);
+  napon = ((float)ulaz / 1023.f) * 5.f;*/
+  //koljeno_val = digitalRead(koljeno);
 
-
-  /*Serial.print(ulaz); //Printanje pina 9 (pwm)
-  Serial.print("\n");
-  Serial.print("Napon: ");
-  Serial.print(napon);
-  Serial.print("\n");
-
-  Serial.print("Koljeno: ");  //Printanje pina 5 
-  Serial.print(koljeno_val);
-  Serial.print("\n");*/
-
-  /*if(koljeno_val == 1)
-  {
-    if(last_val != 1)
-      {
-        if(timer.state() != RUNNING)
-          {
-            timer.start();
-          }
-          else{
-            rpm();
-            timer.stop();
-            last_val = 0;
-          }
-
-
-        
-      }
-  }*/
-
+  //Serial.print("a");
 }
 
 void rpm()
 {
-  int ciklus = timer.read() - proslo_vrijeme;
-  float frekvencija = 1 / (ciklus * 1000000);
+  if(timer.state() != RUNNING)
+  {
+    timer.start();
+  }
+  else
+  {
+    timer.stop();
+    uint32_t x = (timer.read() * 1000 *1000 * 60);
+    f_min = 60.f / (float)x;
 
-  proslo_vrijeme = timer.read();
+    Serial.println(f_min);
 
-  Serial.print(frekvencija);
 
+
+    proslo_vrijeme = timer.read();
+  }
+  //Serial.print("n");
 }
