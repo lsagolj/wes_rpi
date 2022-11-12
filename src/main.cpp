be091dd36874eab1b77e5c720a9384393559155f
lsagolj <sagoljluka@gmail.com>
2022-11-12 17:46:36 +0100

    Preinake

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,40 @@
 
 
   const int A = A0;  
+  const int koljeno = 5;
+
   int ulaz = 0;
-  float napon;
+  int koljeno_val;
+  int last_val;
+  float napon = 0;
 
 void setup() 
 {
-
+  pinMode(koljeno, INPUT); 	
   pinMode(A, INPUT); 	
-   Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() 
 {
   ulaz = analogRead(A);
-  napon = ulaz / 1023 * 5;
+  napon = ((float)ulaz / 1023.f) * 5.f;
+  koljeno_val = digitalRead(koljeno);
 
-  Serial.print(ulaz);
+
+  /*Serial.print(ulaz); //Printanje pina 9 (pwm)
   Serial.print("\n");
   Serial.print("Napon: ");
   Serial.print(napon);
   Serial.print("\n");
 
-  delay(100);
+  Serial.print("Koljeno: ");  //Printanje pina 5 
+  Serial.print(koljeno_val);
+  Serial.print("\n");*/
+
+  if(koljeno_val == 1)
+  {
+    if(last_val == 1)
+  }
+
 }
