f4e7484fbdfa7b05ad5aabe8bd093821e33213e7
lsagolj <sagoljluka@gmail.com>
2022-11-12 16:36:20 +0100

    Serial print map pina te izracunat napon (ne radi)

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
 #include <Arduino.h>
 
+
+  const int A = A0;  
+  int ulaz = 0;
+  float napon;
+
 void setup() 
 {
+
+  pinMode(A, INPUT); 	
    Serial.begin(9600);
 }
 
 void loop() 
 {
-  Serial.print("Test");
+  ulaz = analogRead(A);
+  napon = ulaz / 1023 * 5;
+
+  Serial.print(ulaz);
+  Serial.print("\n");
+  Serial.print("Napon: ");
+  Serial.print(napon);
+  Serial.print("\n");
 
   delay(100);
 }
