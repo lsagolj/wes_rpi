81c09c5ec7d226ea054e6f8fb5be46ac553dc1df
lsagolj <sagoljluka@gmail.com>
2022-11-13 08:11:31 +0100

    paljenje bobine

--- src/bobina/bobina.cpp
+++ src/bobina/bobina.cpp
@@ -1,7 +1,74 @@
-#include "bobina.h"
-#include "koljenasto/Koljenasto.h"
+#include "Bobina.h"
 
-/*Serial.println(koljenasto->getRPM());
-  Serial.println(bregasta->getRPM());
-  Serial.println(mapSensor->getPressure());*/
+uint32_t tablica[10][5] = {
+{18, 25, 34, 38, 42},
+{15, 23, 30, 40, 40},
+{14, 21, 28, 38, 38},
+{14, 20, 25, 36, 36},
+{13, 20, 24, 35, 35},
+{13, 19, 24, 33, 33},
+{13, 19, 23, 32, 32},
+{13, 19, 22, 30, 30},
+{13, 18, 21, 28, 28},
+{13, 18, 20, 25, 25},
+};
 
+/*  {13, 13, 13, 13, 13, 13, 14, 14, 15, 18},
+    {18, 18, 19, 19, 19, 20, 20, 21, 23, 25},
+    {20, 21, 22, 23, 24, 24, 25, 28, 30, 34},
+    {25, 28, 30, 32, 33, 35, 36, 38, 40, 38},
+    {25, 28, 30, 32, 33, 35, 36, 38, 40, 42}*/
+
+Bobina::Bobina() {
+
+}
+
+uint32_t Bobina::getAngle(uint32_t pressure, uint32_t rpm) {
+    if (rpm == 0)
+        return 0;
+
+    if (rpm < 1200) {
+        return 10;
+    } else if (rpm < 1600) {
+        return tablica[getPaInt(pressure)][0];
+    } else if (rpm < 2150) {
+        return tablica[getPaInt(pressure)][1];
+    } else if (rpm < 2750) {
+        return tablica[getPaInt(pressure)][2];
+    } else if (rpm < 3500) {
+        return tablica[getPaInt(pressure)][3];
+    } else {
+        return tablica[getPaInt(pressure)][4];
+    }
+}
+
+uint32_t Bobina::getPaInt(uint32_t pressure) {
+    if (pressure < 20) {
+        return 0;
+    } else if (pressure < 28) {
+        return 1;
+    } else if (pressure < 35) {
+        return 2;
+    } else if (pressure < 45) {
+        return 3;
+    } else if (pressure < 55) {
+        return 4;
+    } else if (pressure < 65) {
+        return 5;
+    } else if (pressure < 75) {
+        return 6;
+    } else if (pressure < 85) {
+        return 7;
+    } else if (pressure < 95) {
+        return 8;
+    } else {
+        return 9;
+    }
+}
+
+void Bobina::sendPulse() {
+
+    //digitalWrite(7, 1);         //Stavlja pin 7 u val 1
+    Serial.println("Nabijam se");
+    delayMicroseconds(1500);    //Necmo irl koristiti delay al ovo tek tolko da vidimo kako radi   
+}
--- src/bobina/bobina.h
+++ src/bobina/bobina.h
@@ -1,13 +1,11 @@
 #pragma once
 #include <Arduino.h>
 
-#define punjenje_us 1500 //Trajanje od 1.5 ms
-
-
-unsigned long s_LastReadTime;
-unsigned long s_CurrDeltaTime;
-unsigned long s_LastDeltaTime;
-
-
-
-void saljiBobinu();
+class Bobina {
+public:
+    Bobina();
+    uint32_t getAngle(uint32_t pressure, uint32_t rpm);
+    
+private:
+    uint32_t getPaInt(uint32_t pressure);
+};
